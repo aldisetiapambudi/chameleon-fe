437b05552b08bfdc64318bdaf3596f9514404b4d
Zaenurrochman <zaenur.rochman98@gmail.com>
2022-06-12 16:13:24 +0700

    init models

---
+++ app/Models/PreRelease.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PreRelease extends Model
+{
+    use HasFactory;
+}
